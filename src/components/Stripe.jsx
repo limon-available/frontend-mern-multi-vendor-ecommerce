72dba37c455c1a9257601d9ff56d9ab1a089fa0e
Nazmul Limon <nazmulhossenlimon56@gmail.com>
2025-10-05 12:23:38 +0600

    deploy update1

---
+++ src/components/Stripe.jsx
@@ -0,0 +1,42 @@
+ import React, { useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js' 
+import { Elements } from '@stripe/react-stripe-js' 
+import axios from 'axios';
+import CheckoutForm from './CheckoutForm';
+const stripePromise = loadStripe('pk_test_51S5MXsBezsUpnBx3xqoKn2fNwm2haJQJLXs2tHbc8El265abcMLcLhUaQ1PkUlfpg4KEKWALNtSMLhsj49joUdKH00eH4gkZ2i')
+const stripe = await stripePromise
+console.log(stripe)
+
+const Stripe = ({ price, orderId }) => {
+    const [clientSecret, setClientSecret] = useState('')
+    const appearance = {
+        theme: 'stripe'
+    }
+    const options = {
+        appearance,
+        clientSecret
+    }
+
+    const create_payment = async () => {
+        try {
+            const { data } = await axios.post('http://localhost:5000/api/order/create-payment',{price},{withCredentials:true})
+            setClientSecret(data.clientSecret)
+        } catch (error) {
+            console.log(error.response.data)
+        }
+    }
+
+    return (
+        <div className='mt-4'>
+            {
+                clientSecret ? (
+                    <Elements options={options} stripe={stripePromise}>
+                        <CheckoutForm orderId={orderId} />
+                    </Elements>
+                ) : <button onClick={create_payment} className='px-10 py-[6px] rounded-sm hover:shadow-green-700/30 hover:shadow-lg bg-green-700 text-white'>Start Payment</button>
+            }
+        </div>
+    );
+}; 
+
+export default Stripe;
