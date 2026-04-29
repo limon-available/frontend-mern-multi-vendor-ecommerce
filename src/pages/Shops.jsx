ec3ef6377b72f2d7fd61041da3bbdc59e568971c
Nazmul Limon <nazmulhossenlimon56@gmail.com>
2026-04-29 12:57:07 +0600

    upload-cloudinary

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -85,7 +85,12 @@ const Shops = () => {
   return (
     <div>
       <Header />
-      <section className='bg-[url("/images/banner/shop.png")] h-[220px] mt-6 bg-cover bg-no-repeat relative bg-left'>
+      <section
+        style={{
+          backgroundImage: `url("https://res.cloudinary.com/dnjrmakcu/image/upload/v1777445425/shop_gs5xsy.png")`,
+        }}
+        className="h-[220px] mt-6 bg-cover bg-no-repeat relative bg-left"
+      >
         <div className="absolute left-0 top-0 w-full h-full bg-[#2422228a]">
           <div className="w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto">
             <div className="flex flex-col justify-center gap-1 items-center h-full w-full text-white">
